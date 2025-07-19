65a7d46c0c0c7a768e298387a093785829cbc66c
Yared Assefa <tamene1970@gmail.com>
2025-07-19 10:59:38 +0200

    Coded Pick Flight section with the help of chatGPT40

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,23 +2,30 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
 
-    # Only collect dates from existing flights, and sort them
-    @available_dates = Flight.select(:start_datetime)
-                             .distinct
-                             .map { |f| f.start_datetime.to_date }
+    # Efficiently gather unique flight dates (sorted)
+    @available_dates = Flight.distinct
+                             .pluck(:start_datetime)
+                             .map(&:to_date)
                              .uniq
                              .sort
 
+    # If all 3 search inputs are present
     if params[:departure_airport_id].present? &&
        params[:arrival_airport_id].present? &&
        params[:date].present?
 
-      date = Date.parse(params[:date])
+      # Prevent invalid searches where departure == arrival
+      if params[:departure_airport_id] == params[:arrival_airport_id]
+        flash.now[:alert] = "Departure and arrival airports must be different."
+        @flights = []
+      else
+        date = Date.parse(params[:date])
 
-      @flights = Flight.where(
-        departure_airport_id: params[:departure_airport_id],
-        arrival_airport_id: params[:arrival_airport_id]
-      ).where(start_datetime: date.beginning_of_day..date.end_of_day)
+        @flights = Flight.where(
+          departure_airport_id: params[:departure_airport_id],
+          arrival_airport_id: params[:arrival_airport_id]
+        ).where(start_datetime: date.beginning_of_day..date.end_of_day)
+      end
     else
       @flights = []
     end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
    # Defines the root path route ("/")
    root "flights#index"
   resources :flights, only: [ :index ]
+
+  resources :bookings, only: [ :new, :create ]
 end
